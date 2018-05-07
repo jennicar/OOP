b03db5762070f870b747b8f6d99c22e0dd0b3c44
jennicar <jlramones@mavs.coloradomesa.edu>
2018-05-07 13:59:17 -0600

    moved files around

--- DoD/include/Mode.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-enum Mode { PAUSE, PLAY };
--- Task/Task/Task.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class Task{
-	public: Task();
-	public:	~Task();
-	public:	void addTask();
-	public:	void removeTask();
-};
-
